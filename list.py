847b30c6dce8509a1173f6932c97079b48469130
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2024-11-25 09:06:03 +0300

    list practise

---
+++ list.py
@@ -0,0 +1,27 @@
+# Create an empty list
+my_list = []
+
+# Append elements to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert the value 15 at the second position
+my_list.insert(1, 15)
+
+another_list =[50,60,70]
+
+# Extend the list with the elements of another_list
+my_list.extend(another_list)
+# print (my_list)  
+
+my_list.pop(-1)
+# print(my_list)
+my_list.sort()
+# print the index of 30
+
+print(my_list.index(30))
+
+
+
